3f9838c51990534dc96fe1c41ff7e8920bcdb750
Glen <glen@jamdirect.ca>
2016-09-29 12:55:45 -0400

    Fixed contact form

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = '';
-$sendTo = 'Demo contact form <glenrhodges@gmail.com>';
+$from = $_REQUEST['email'];;
+$sendTo = 'glenrhodges@gmail.com';
 $subject = 'New message from glenhodgesweb.ninja';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
