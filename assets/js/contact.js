0988fe1bcabc5879c748f43bdf5c32d20474a7c8
Glen Hodges <glen.hodges@wiseandhammer.com>
2018-08-07 06:15:40 -0400

    Commit before I change contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,110 +1,29 @@
-// $(function () {
-//   "use strict";
+$(function () {
+  "use strict";
 
-//     $('#contact-form').validator();
+    $('#contact-form').validator();
 
-//     $('#contact-form').on('submit', function (e) {
-//         if (!e.isDefaultPrevented()) {
-//             var url = "//formspree.io/glenrhodges@gmail.com";
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "assets/php/contact.php";
 
-//             $.ajax({
-//                 url: "//formspree.io/glenrhodges@gmail.com",
-//                 method: "POST",
-//                 data: {
-//                     name: name,
-//                     _replyto: email,
-//                     email: email,
-//                     comments: comments,
-//                     _subject: "My Form Submission"
-//                 },
-//                 dataType: "json",
-//                 success: function (data)
-//                 {
-//                     console.log("success");
-//                     var messageAlert = 'alert-' + data.type;
-//                     var messageText = data.message;
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
 
-//                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-//                     if (messageAlert && messageText) {
-//                         $('#contact-form').find('.messages').html(alertBox);
-//                         $('#contact-form')[0].reset();
-//                     }
-//                 }
-//             });
-//             return false;
-//         }
-//     })
-// });
-
-// $(function () {
-//     "use strict";
-
-//     $('#contact-form').validator();
-
-//     $("#contact-form").on("submit", function(e) {
-//         e.preventDefault();
-
-//         var url = "//formspree.io/glenrhodges@gmail.com";
-    
-//         var name = $("#form_name").val();
-//         var lastName = $("#form_lastname").val();
-//         var email = $("#form_email").val();
-//         var phone = $("#form_phone").val();
-//         var message = $("#form_message").val();
-    
-//         //pretend we don't need validation
-    
-//         //send to formspree
-//         $.ajax({
-//         url: url,
-//         method: "POST",
-//         data: {
-//             name: name,
-//             lastName: lastName,
-//             phone: phone,
-//             _replyto: email,
-//             email: email,
-//             message: message,
-//             _subject: "My Form Submission"
-//         },
-//         dataType: "json",
-//         success: function() {
-//             console.log("success");
-//             $("#formBlock").hide();
-//             $("#thankyouBlock").show();
-//         }
-//         });
-//     });
-// }); // END FUNCTION
-
-$("#contact-form).on("submit", function(e) {
-    e.preventDefault();
-  
-    //get the name field value
-    var name = $("#form_name").val();
-    //get the name field value
-    var email = $("#form_email").val();
-    //get the comments
-    var message = $("#form_message").val();
-  
-    //pretend we don't need validation
-  
-    //send to formspree
-    $.ajax({
-      url: "//formspree.io/glenrhodges@gmail.com",
-      method: "POST",
-      data: {
-        name: name,
-        _replyto: email,
-        email: email,
-        message: message,
-        _subject: "My Form Submission"
-      },
-      dataType: "json",
-      success: function() {
-        console.log("success");
-        $("#formBlock").hide();
-        $("#thankyouBlock").show();
-      }
-    });
-  });
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
