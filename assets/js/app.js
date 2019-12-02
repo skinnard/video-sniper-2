505c3bd4f180839f5265ebfefc675da211785ffd
glenhodges <glen@rankhigher.ca>
2019-12-02 08:45:35 -0500

    Finished putting sections in, still need content and copy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
-/* Template Name: Landrick - Saas & Software Landing Page Template
-   Author: Shreethemes
-   E-mail: shreethemes@gmail.com
-   Version: 1.0.0
-   Created: August 2019
-   File Description: Main JS file of the template
-*/
 
 /****************************/
 /*         INDEX            */
@@ -15,96 +8,127 @@
  *     03.  Back to top     *
  ===========================*/
 
-! function($) {
-    "use strict"; 
-    // Loader 
-    $(window).on('load', function() {
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    }); 
-    
-    // Menu
-    $('.navbar-toggle').on('click', function (event) {
-        $(this).toggleClass('open');
-        $('#navigation').slideToggle(400);
-    });
-    
-    $('.navigation-menu>li').slice(-1).addClass('last-elements');
-    
-    $('.menu-arrow,.submenu-arrow').on('click', function (e) {
-        if ($(window).width() < 992) {
-            e.preventDefault();
-            $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
-        }
-    });
-    
-    $(".navigation-menu a").each(function () {
-        if (this.href == window.location.href) {
-            $(this).parent().addClass("active"); 
-            $(this).parent().parent().parent().addClass("active"); 
-            $(this).parent().parent().parent().parent().parent().addClass("active"); 
-        }
-    });
+! function ( $ ) {
+	"use strict";
+	// Loader 
+	$( window ).on( 'load', function () {
+		$( '#status' ).fadeOut();
+		$( '#preloader' ).delay( 350 ).fadeOut( 'slow' );
+		$( 'body' ).delay( 350 ).css( {
+			'overflow': 'visible'
+		} );
+	} );
 
-    // Clickable Menu
-    $(".has-submenu a").click(function() {
-        if(window.innerWidth < 992){
-            if($(this).parent().hasClass('open')){
-                $(this).siblings('.submenu').removeClass('open');
-                $(this).parent().removeClass('open');
-            } else {
-                $(this).siblings('.submenu').addClass('open');
-                $(this).parent().addClass('open');
-            }
-        }
-    });
+	// Menu
+	$( '.navbar-toggle' ).on( 'click', function ( event ) {
+		$( this ).toggleClass( 'open' );
+		$( '#navigation' ).slideToggle( 400 );
+	} );
 
-    $('.mouse-down').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 0
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	$( '.navigation-menu>li' ).slice( -1 ).addClass( 'last-elements' );
 
-    //Sticky
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+	$( '.menu-arrow,.submenu-arrow' ).on( 'click', function ( e ) {
+		if ( $( window ).width() < 992 ) {
+			e.preventDefault();
+			$( this ).parent( 'li' ).toggleClass( 'open' ).find( '.submenu:first' ).toggleClass( 'open' );
+		}
+	} );
 
-        if (scroll >= 50) {
-            $(".sticky").addClass("nav-sticky");
-        } else {
-            $(".sticky").removeClass("nav-sticky");
-        }
-    });
+	$( ".navigation-menu a" ).each( function () {
+		if ( this.href == window.location.href ) {
+			$( this ).parent().addClass( "active" );
+			$( this ).parent().parent().parent().addClass( "active" );
+			$( this ).parent().parent().parent().parent().parent().addClass( "active" );
+		}
+	} );
 
-    // Back to top
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn();
-        } else {
-            $('.back-to-top').fadeOut();
-        }
-    }); 
-    $('.back-to-top').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 3000);
-        return false;
-    }); 
+	// Clickable Menu
+	$( ".has-submenu a" ).click( function () {
+		if ( window.innerWidth < 992 ) {
+			if ( $( this ).parent().hasClass( 'open' ) ) {
+				$( this ).siblings( '.submenu' ).removeClass( 'open' );
+				$( this ).parent().removeClass( 'open' );
+			} else {
+				$( this ).siblings( '.submenu' ).addClass( 'open' );
+				$( this ).parent().addClass( 'open' );
+			}
+		}
+	} );
 
-    //Tooltip
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    });
-    //Popover
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    });
+	$( '.mouse-down' ).on( 'click', function ( event ) {
+		var $anchor = $( this );
+		$( 'html, body' ).stop().animate( {
+			scrollTop: $( $anchor.attr( 'href' ) ).offset().top - 0
+		}, 1500, 'easeInOutExpo' );
+		event.preventDefault();
+	} );
 
-    // AOS.init({
-    //     easing: 'ease-in-out-sine',
-    //     duration: 1000
-    // });
-}(jQuery)
+	//Sticky
+	$( window ).scroll( function () {
+		var scroll = $( window ).scrollTop();
+
+		if ( scroll >= 50 ) {
+			$( ".sticky" ).addClass( "nav-sticky" );
+		} else {
+			$( ".sticky" ).removeClass( "nav-sticky" );
+		}
+	} );
+
+	// Back to top
+	$( window ).scroll( function () {
+		if ( $( this ).scrollTop() > 100 ) {
+			$( '.back-to-top' ).fadeIn();
+		} else {
+			$( '.back-to-top' ).fadeOut();
+		}
+	} );
+	$( '.back-to-top' ).click( function () {
+		$( "html, body" ).animate( { scrollTop: 0 }, 3000 );
+		return false;
+	} );
+
+	//Tooltip
+	$( function () {
+		$( '[data-toggle="tooltip"]' ).tooltip()
+	} );
+	//Popover
+	$( function () {
+		$( '[data-toggle="popover"]' ).popover()
+	} );
+
+	// AOS.init({
+	//     easing: 'ease-in-out-sine',
+	//     duration: 1000
+	// });
+
+	// Pricing monthly switch
+	var e = document.getElementById( "filt-monthly" ),
+		d = document.getElementById( "filt-hourly" ),
+		t = document.getElementById( "switcher" ),
+		m = document.getElementById( "monthly" ),
+		y = document.getElementById( "hourly" );
+
+	e.addEventListener( "click", function () {
+		t.checked = false;
+		e.classList.add( "toggler--is-active" );
+		d.classList.remove( "toggler--is-active" );
+		m.classList.remove( "hide" );
+		y.classList.add( "hide" );
+	} );
+
+	d.addEventListener( "click", function () {
+		t.checked = true;
+		d.classList.add( "toggler--is-active" );
+		e.classList.remove( "toggler--is-active" );
+		m.classList.add( "hide" );
+		y.classList.remove( "hide" );
+	} );
+
+	t.addEventListener( "click", function () {
+		d.classList.toggle( "toggler--is-active" );
+		e.classList.toggle( "toggler--is-active" );
+		m.classList.toggle( "hide" );
+		y.classList.toggle( "hide" );
+	} )
+
+}( jQuery )
