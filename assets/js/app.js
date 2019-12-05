00edfd08a92873b3a990a85fc4cefbd876ec9578
glenhodges <glen@rankhigher.ca>
2019-12-05 09:10:57 -0500

    Figuring out accouding icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -101,34 +101,37 @@
 	//     duration: 1000
 	// });
 
-	// Pricing monthly switch
-	var e = document.getElementById( "filt-monthly" ),
-		d = document.getElementById( "filt-hourly" ),
-		t = document.getElementById( "switcher" ),
-		m = document.getElementById( "monthly" ),
-		y = document.getElementById( "hourly" );
-
-	e.addEventListener( "click", function () {
-		t.checked = false;
-		e.classList.add( "toggler--is-active" );
-		d.classList.remove( "toggler--is-active" );
-		m.classList.remove( "hide" );
-		y.classList.add( "hide" );
-	} );
-
-	d.addEventListener( "click", function () {
-		t.checked = true;
-		d.classList.add( "toggler--is-active" );
-		e.classList.remove( "toggler--is-active" );
-		m.classList.add( "hide" );
-		y.classList.remove( "hide" );
-	} );
-
-	t.addEventListener( "click", function () {
-		d.classList.toggle( "toggler--is-active" );
-		e.classList.toggle( "toggler--is-active" );
-		m.classList.toggle( "hide" );
-		y.classList.toggle( "hide" );
-	} )
-
 }( jQuery )
+
+// Pricing monthly switch
+var e = document.getElementById( "filt-monthly" ),
+	d = document.getElementById( "filt-yearly" ),
+	t = document.getElementById( "switcher" ),
+	m = document.getElementById( "monthly" ),
+	y = document.getElementById( "yearly" );
+
+console.log( 'Monthly', m );
+
+e.addEventListener( "click", function () {
+	console.log( 'filt-monthly' );
+	t.checked = false;
+	e.classList.add( "toggler--is-active" );
+	d.classList.remove( "toggler--is-active" );
+	m.classList.remove( "hide" );
+	y.classList.add( "hide" );
+} );
+
+d.addEventListener( "click", function () {
+	t.checked = true;
+	d.classList.add( "toggler--is-active" );
+	e.classList.remove( "toggler--is-active" );
+	m.classList.add( "hide" );
+	y.classList.remove( "hide" );
+} );
+
+t.addEventListener( "click", function () {
+	d.classList.toggle( "toggler--is-active" );
+	e.classList.toggle( "toggler--is-active" );
+	m.classList.toggle( "hide" );
+	y.classList.toggle( "hide" );
+} )
