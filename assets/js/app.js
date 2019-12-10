847181c6009f088126454237ff5fd97b8c725ab1
glenhodges <glen@rankhigher.ca>
2019-12-09 22:53:55 -0500

    Added smooth scrolling and scroll spy links to home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,19 +20,19 @@
 	} );
 
 	// Menu
-	// $( '.navbar-toggle' ).on( 'click', function ( event ) {
-	// 	$( this ).toggleClass( 'open' );
-	// 	$( '#navigation' ).slideToggle( 400 );
-	// } );
+	$( '.navbar-toggle' ).on( 'click', function ( event ) {
+		$( this ).toggleClass( 'open' );
+		$( '#navigation' ).slideToggle( 400 );
+	} );
 
 	// $( '.navigation-menu>li' ).slice( -1 ).addClass( 'last-elements' );
 
-	// $( '.menu-arrow,.submenu-arrow' ).on( 'click', function ( e ) {
-	// 	if ( $( window ).width() < 992 ) {
-	// 		e.preventDefault();
-	// 		$( this ).parent( 'li' ).toggleClass( 'open' ).find( '.submenu:first' ).toggleClass( 'open' );
-	// 	}
-	// } );
+	$( '.menu-arrow,.submenu-arrow' ).on( 'click', function ( e ) {
+		if ( $( window ).width() < 992 ) {
+			e.preventDefault();
+			$( this ).parent( 'li' ).toggleClass( 'open' ).find( '.submenu:first' ).toggleClass( 'open' );
+		}
+	} );
 
 	// $( ".navigation-menu a" ).each( function () {
 	// 	if ( this.href == window.location.href ) {
@@ -43,25 +43,25 @@
 	// } );
 
 	// Clickable Menu
-	// $( ".has-submenu a" ).click( function () {
-	// 	if ( window.innerWidth < 992 ) {
-	// 		if ( $( this ).parent().hasClass( 'open' ) ) {
-	// 			$( this ).siblings( '.submenu' ).removeClass( 'open' );
-	// 			$( this ).parent().removeClass( 'open' );
-	// 		} else {
-	// 			$( this ).siblings( '.submenu' ).addClass( 'open' );
-	// 			$( this ).parent().addClass( 'open' );
-	// 		}
-	// 	}
-	// } );
+	$( ".has-submenu a" ).click( function () {
+		if ( window.innerWidth < 992 ) {
+			if ( $( this ).parent().hasClass( 'open' ) ) {
+				$( this ).siblings( '.submenu' ).removeClass( 'open' );
+				$( this ).parent().removeClass( 'open' );
+			} else {
+				$( this ).siblings( '.submenu' ).addClass( 'open' );
+				$( this ).parent().addClass( 'open' );
+			}
+		}
+	} );
 
-	// $( '.mouse-down' ).on( 'click', function ( event ) {
-	// 	var $anchor = $( this );
-	// 	$( 'html, body' ).stop().animate( {
-	// 		scrollTop: $( $anchor.attr( 'href' ) ).offset().top - 0
-	// 	}, 1500, 'easeInOutExpo' );
-	// 	event.preventDefault();
-	// } );
+	$( '.mouse-down' ).on( 'click', function ( event ) {
+		var $anchor = $( this );
+		$( 'html, body' ).stop().animate( {
+			scrollTop: $( $anchor.attr( 'href' ) ).offset().top - 0
+		}, 1500, 'easeInOutExpo' );
+		event.preventDefault();
+	} );
 
 	//Sticky
 	$( window ).scroll( function () {
@@ -124,6 +124,43 @@
 	regRegisterBtn.addEventListener( 'click', function ( e ) {
 		regCard.classList.toggle( 'is-flipped' );
 	} );
+
+	// Select all links with hashes
+	$( 'a[href*="#"]' )
+		// Remove links that don't actually link to anything
+		.not( '[href="#"]' )
+		.not( '[href="#0"]' )
+		.click( function ( event ) {
+			// On-page links
+			if (
+				location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' )
+				&&
+				location.hostname == this.hostname
+			) {
+				// Figure out element to scroll to
+				var target = $( this.hash );
+				target = target.length ? target : $( '[name=' + this.hash.slice( 1 ) + ']' );
+				// Does a scroll target exist?
+				if ( target.length ) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					$( 'html, body' ).animate( {
+						scrollTop: target.offset().top
+					}, 1000, function () {
+						// Callback after animation
+						// Must change focus!
+						var $target = $( target );
+						$target.focus();
+						if ( $target.is( ":focus" ) ) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr( 'tabindex', '-1' ); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						};
+					} );
+				}
+			}
+		} );
 
 }( jQuery )
 
